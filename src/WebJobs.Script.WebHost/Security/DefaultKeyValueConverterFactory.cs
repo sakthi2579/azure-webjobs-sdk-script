48860e7e6a7ee741943b33413c4c5faca2d4c84f
Fabio Cavalcante <fabio.cavalcante@microsoft.com>
2016-09-08 18:27:42 -0700

    Removing encryption stuff

--- src/WebJobs.Script.WebHost/Security/DefaultKeyValueConverterFactory.cs
+++ src/WebJobs.Script.WebHost/Security/DefaultKeyValueConverterFactory.cs
@@ -11,41 +11,11 @@ namespace Microsoft.Azure.WebJobs.Script.WebHost
 {
     public sealed class DefaultKeyValueConverterFactory : IKeyValueConverterFactory
     {
-        private bool _encryptionSupported;
-
-        public DefaultKeyValueConverterFactory()
-        {
-            _encryptionSupported = Environment.GetEnvironmentVariable(AesCryptoKeyValueConverter.DefaultEncryptionKeyId) != null;
-        }
+        private static readonly PlainTextKeyValueConverter PlainTextConverter = new PlainTextKeyValueConverter();
 
         public IKeyValueConverter GetValueConverter(Key key, KeyConversionAction action)
         {
-            if (action == KeyConversionAction.Read)
-            {
-                return GetReadValueConverter(key);
-            }
-
-            return GetWriteValueConverter(key);
-        }
-
-        private IKeyValueConverter GetWriteValueConverter(Key key)
-        {
-            if (_encryptionSupported)
-            {
-                return new AesCryptoKeyValueConverter();
-            }
-
-            return new PlainTextKeyValueConverter();
-        }
-
-        private IKeyValueConverter GetReadValueConverter(Key key)
-        {
-            if (key.IsEncrypted)
-            {
-                return new AesCryptoKeyValueConverter();
-            }
-
-            return new PlainTextKeyValueConverter();
+            return PlainTextConverter;
         }
     }
 }
